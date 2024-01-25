55d8b781889af080be33d62b44dbf8bd93a1a7e5
Roman177-png <miron-2002@ukr.net>
2024-01-25 09:21:14 +0200

    Main page

---
+++ src/views/product/productIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       Products 
+    </div>
+</template>
+<script>
+export default{
+    name:"Index",
+    mounted(){
+        $(document).trigger('change')
+    }
+}
+</script>
+<style scoped>
+
+</style>
